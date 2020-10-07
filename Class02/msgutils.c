e0a0caf120e45eac89e419cfde12e79ec6bba738
joaocostaifg <joaocosta.tra@outlook.com>
2020-10-07 10:08:56 +0100

    Add: calc bcc.

--- Class02/msgutils.c
+++ Class02/msgutils.c
@@ -6,3 +6,8 @@ void fillByteField(unsigned char *buf, enum byteField field,
                    unsigned char byte) {
   buf[field] = byte;
 }
+
+void setBCCField(unsigned char *buf) {
+  unsigned char bcc = buf[A] ^ buf[C];
+  fillByteField(buf, BCC, bcc);
+}
