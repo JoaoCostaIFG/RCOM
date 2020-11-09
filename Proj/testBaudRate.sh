feef21917fe402cc9aca963e22fc7231d7c84061
Joao Lucas <joaolucassilvamartins@gmail.com>
2020-11-09 16:49:17 +0000

    AAHAHAHHAHAHAHAHHAHAHA

--- Proj/testBaudRate.sh
+++ Proj/testBaudRate.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
 
-#function cleanup {
-    #echo "killing all childs"
-    #pkill -P $$
-#}
-
-#trap cleanup EXIT
-#declare -a baudRates=(9600 19200 38400 57600 115200 230400 460800)
-
-#for baudRate in ${baudRates[*]}; do
-    #./rcom_tp1 -s TRANSMITTER -p 0 -b "$baudRate" -f files/gato.gif > transLog &
-    #log=$(./rcom_tp1 -s RECEIVER -p 1 -b "$baudRate")
-    #wait
-    #echo "$log"
-    #sleep 0.5
-#done
-#!/bin/sh
-
 serial0="10"
 serial1="11"
 
 baudrates="9600 19200 38400 57600 115200 230400 460800"
 
-for br in $baudrates; do
-  ./rcom_tp1 -s RECEIVER -p "$serial1" -b "$br" 2>&1 > /dev/null &
-  p="$!"
-  log=$(./rcom_tp1 -s TRANSMITTER -p "$serial0" -b "$br" -f files/gato.gif 2>&1)
-  wait "$p"
-  echo $log | awk -F'Connection duration: ' '{print $2}' | awk '{print $1}'
-done
+#sshpass -p torvalds ssh -t netedu@192.168.109.13 "mkdir /home/netedu/test"
+
+if [ "$1" == "local" ]; then
+    for br in $baudrates; do
+      ./rcom_tp1 -s RECEIVER -p "$serial1" -b "$br" 2>&1 &
+      p="$!"
+      log=$(./rcom_tp1 -s TRANSMITTER -p "$serial0" -b "$br" -f files/gato.gif 2>&1)
+      wait "$p"
+      echo $log | awk -F'Connection duration: ' '{print $2}' | awk '{print $1}'
+    done
+
+elif [ "$1" == "feup" ]; then
+    if [ "$2" == "RC" ]; then
+        for br in $baudrates; do
+            ./rcom_tp1 -s RECEIVER -p "$serial1" -b "$br"
+
+        done
+    elif [ "$2" == "TR" ]; then
+        echo "" > res
+        for br in $baudrates; do
+            log=$(./rcom_tp1 -s TRANSMITTER -p "$serial0" -b "$br" -f files/gato.gif 2>&1)
+            time=$(echo $log | awk -F'Connection duration: ' '{print $2}' | awk '{print $1}')
+            printf "%s\t%s\n" $br $time >> res
+        done
+    fi
+fi
+
